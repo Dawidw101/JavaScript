c63af7c0af788b092572f3952fcb3ac84b33c67e
Dawid <76258206+Dawidw101@users.noreply.github.com>
2023-01-09 10:06:54 +0100

    Add files via upload

---
+++ tree_unfinished/app.js
@@ -0,0 +1,147 @@
+// const text1 = 'powiększ mnie!'
+// const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+// const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+// const text4 = 'sprawdź, czy zawieram słowo "czy"'
+// const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+// const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+// const text7 = 'podziel, ten, string, od, przecinków'
+
+
+// console.log(text1.toUpperCase())
+// console.log(text2.toLowerCase())
+// console.log(text3.slice(6))
+// console.log(text4.includes('czy'))
+// console.log(text5.charAt(2))
+// console.log(text6.replaceAll('pies', 'kot'))
+// console.log(text7.split(','))
+
+
+
+
+// const x = parseInt(prompt("podaj x"))
+// const y = parseInt(prompt("podaj y"))
+
+// if (x > y)
+// {
+//     console.log(`${x} jest większe niż ${y}`)
+// }
+// if (y > x)
+// {
+//     console.log(`${y} jest większe niż ${x}`)
+// }
+// if (y == x)
+// {
+//     console.log(`${y} i ${x} to tyle samo`)
+// }
+// else
+// {
+//     console.log(`Something is not yes`)
+// }
+
+
+
+
+
+// const discount = parseInt(prompt('Ile dzisiaj % zniżki?'))
+
+// switch (discount) {
+
+//   case 20:
+//     console.log (`dzisiaj jest 20% zniżki`)
+//     break;
+
+//     case 50:
+//     console.log (`dzisiaj jest 50% zniżki`)
+//     break;
+
+//     case 70:
+//     console.log (`dzisiaj jest 70% zniżki`)
+//     break;
+
+//   default:
+//     console.log(`Dzisiaj niestandardowa promocja - ${discount}% zniżki`);
+// }
+
+
+
+
+// const x = parseInt(prompt("podaj x"))
+
+// if (x % 2 == 0)
+// {
+//     console.log("parzysta")
+// }
+// else
+// {
+//     console.log("nieparzysta")
+// }
+
+
+
+
+// for (let i = 0; i <= 10; i++)
+// {
+//     console.log(i)
+// }
+
+
+
+
+// const colors = ["blue", "green", "pink", "red", "purple", "orange"]
+
+// for (let i = 0; i < colors.length; i++)
+// {
+//     console.log(colors[i])
+// }
+
+
+
+// let i = 0
+
+// while (i <= 5)
+// {
+//     console.log(i)
+//     i++
+// }
+
+
+
+// const miasto = ["Olsztyn", "Warszawa", "Kraków", "Poznań", "Wrocław", "Gdańsk"]
+
+// for (i = 0; i < miasto.length; i++)
+// {
+//     console.log(`Nazwa miasta: ${miasto[i].toUpperCase()}`)
+// }
+
+
+
+// let i = 0
+
+// while (i <= 10)
+// {
+//     console.log(i)
+//     i = i + 2
+// }
+
+
+
+// let i = 20
+
+// do {
+//   i = i - 3
+// } while (i > 0)
+
+// console.log(i)
+
+
+
+// const numbers = [5, 8, 10, 23, 48, 60]
+
+// for (const number of numbers) {
+//     if (number % 2 == 0) {
+//         console.log(`Liczba ${number} jest parzysta`)
+//     }
+//     else {
+//         console.log(`Liczba ${number} jest nieparzysta`,)
+//     }
+// }
